b28918046d180b058342b003b6e0829d9e9c3d78
Korbin Sargent <korbinksargent@gmail.com>
2021-09-23 22:13:50 -0600

    building server and added .gitignore

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const apiRoutes = ("./routes/apiRoutes);
+const viewRoutes = ("./routes/viewRoutes")
+
+
+
+const app = express();
+console.log("!!!! Look here !!!!!!" + app);
+const PORT = process.env.PORT;
+
+//connect mongoose here
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
