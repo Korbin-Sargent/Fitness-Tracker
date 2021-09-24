4e329185920a91ea3dfea6d088f9a2a7198934a2
Korbin Sargent <korbinksargent@gmail.com>
2021-09-23 22:50:36 -0600

    Updating wWorkout model

---
+++ models/Workout.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+      },
+      name: {
+        type: String,
+        time: true,
+      },
+      duration: {
+        type: Number,
+        required: true,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+    },
+  ],
+  totalDuration: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const viewRoutes = "./routes/viewRoutes";
 
 const app = express();
 console.log("!!!! Look here !!!!!!" + app);
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 //connect mongoose here
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
@@ -19,3 +19,11 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
+app.use(logger("dev");)
+
+app.use(apiRoutes);
+app.use(viewRoutes);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
